d1a9f22a0a83bde9fb34a5bd33348e1d5c1dd194
yanghao <yanghaoynu@qq.com>
2018-07-03 07:18:02 +0800

    天亮提交

--- src/test/BufferedOutputStreamTest.java
+++ src/test/BufferedOutputStreamTest.java
@@ -28,6 +28,7 @@ public class BufferedOutputStreamTest {
         System.out.println("遗忘就是个");
         System.out.println("你好");
         System.out.println("你好，我是杨昊，请问你是谁啊");
+        System.out.println("100");
 	}
 
 }
