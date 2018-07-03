f779d60a304dfe38d225d49d481776c2950c3d6b
yanghao <yanghaoynu@qq.com>
2018-07-03 14:09:28 +0800

    下午测试

--- src/test/BufferedOutputStreamTest.java
+++ src/test/BufferedOutputStreamTest.java
@@ -11,24 +11,24 @@ import java.io.IOException;
  */
 public class BufferedOutputStreamTest {
 
-	public BufferedOutputStreamTest()  {}
+	public BufferedOutputStreamTest() {
+	}
 
 	public static void main(String[] args) throws IOException {
 		File file = new File("D:\\QLDownload\\BufferedOutputStreamTest.txt");
-	    FileOutputStream fileOutputStream = new FileOutputStream(file,true);
-	    BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
-	    String lineString = "测试BufferedOutputStream\r\n";
-	    for (int i = 0; i <1000000; i++) {
+		FileOutputStream fileOutputStream = new FileOutputStream(file, true);
+		BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+		String lineString = "测试BufferedOutputStream\r\n";
+		for (int i = 0; i < 1000000; i++) {
 			bufferedOutputStream.write(lineString.getBytes(), 0, lineString.getBytes().length);
 		}
-	    bufferedOutputStream.flush();
+		bufferedOutputStream.flush();
 		bufferedOutputStream.close();
 		fileOutputStream.close();
-        System.out.println("this is a test out!");
-        System.out.println("遗忘就是个");
-        System.out.println("你好");
-        System.out.println("你好，我是杨昊，请问你是谁啊");
-        System.out.println("100");
-	}
+		System.out.println("this is a test out!");
+		System.out.println("遗忘就是个");
+		System.out.println("你好");
+		System.out.println("你好，我是杨昊，请问你是谁啊");
 
+	}
 }
